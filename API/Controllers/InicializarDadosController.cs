5f5db729a9fb1dcfb71e9209383eccdcd466f7dd
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2021-11-24 20:04:17 -0300

    Adiciona forma de pagamento no Inicializar

--- API/Controllers/InicializarDadosController.cs
+++ API/Controllers/InicializarDadosController.cs
@@ -26,9 +26,15 @@ namespace API.Controllers
             );
             _context.Produtos.AddRange(new Produto[]
                 {
-                    new Produto { ProdutoId = 1, Nome = "Geladeira Frost-free Electrolux 454L Inverse Cor Inox (DB53X)", Preco = 5629, Quantidade = 1, CategoriaId = 1 },
-                    new Produto { ProdutoId = 2, Nome = "Fogão Consul 5 Bocas Cor Inox Com Mesa de Vidro e Trempe de Ferro Fundido - CFS5VAR", Preco = 1879, Quantidade = 2, CategoriaId = 1 },
-                    new Produto { ProdutoId = 3, Nome = "Microondas Philco 32L Com Porta Espelhada Branco PMO33EB 127V", Preco = 930, Quantidade = 3, CategoriaId = 1 },
+                    new Produto { ProdutoId = 1, Nome = "Geladeira Electrolux 454L DB53X", Preco = 5629, Quantidade = 1, CategoriaId = 1 },
+                    new Produto { ProdutoId = 2, Nome = "Fogão Consul 5 Bocas CFS5VAR", Preco = 1879, Quantidade = 2, CategoriaId = 1 },
+                    new Produto { ProdutoId = 3, Nome = "Microondas Philco 32L PMO33EB", Preco = 930, Quantidade = 3, CategoriaId = 1 },
+                }
+            );
+            _context.FormaPagamentos.AddRange(new FormaPagamento[]
+                {
+                    new FormaPagamento { FormaPagamentoId = 1, Nome = "Cartão de Crédito", Ordem = 1 },
+                    new FormaPagamento { FormaPagamentoId = 2, Nome = "PIX", Ordem = 2 }
                 }
             );
             _context.SaveChanges();
