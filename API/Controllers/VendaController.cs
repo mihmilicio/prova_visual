e6b74760617b1211d1a7587e86aec16a1d714b2e
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2021-11-24 20:35:20 -0300

    Ex3: Adiciona forma de pagamento na venda e finalização (create)

--- API/Controllers/VendaController.cs
+++ API/Controllers/VendaController.cs
@@ -15,6 +15,17 @@ namespace API.Controllers
             _context = context;
         }
 
+        //POST: api/venda/create
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create([FromBody] Venda venda)
+        {
+            venda.FormaPagamento = _context.FormaPagamentos.Find(venda.FormaPagamentoId);
+            _context.Vendas.Add(venda);
+            _context.SaveChanges();
+            return Created("", venda);
+        }
+
         //GET: api/venda/list
         //ALTERAR O MÉTODO PARA MOSTRAR TODOS OS DADOS DA VENDA E OS DADOS RELACIONADOS
         [HttpGet]
